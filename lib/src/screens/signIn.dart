da791b6bd35f798539208851cbf84238e6ce7d71
safiya <safichrif96@gmail.com>
2022-02-16 20:38:25 +0000

    test

--- lib/src/screens/signIn.dart
+++ lib/src/screens/signIn.dart
@@ -7,63 +7,60 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
- 
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   var key = GlobalKey();
-   TextEditingController _email =  TextEditingController(text: 'a@b.com'), _password = TextEditingController(text: '12345678');
+  TextEditingController _email = TextEditingController(text: 'a@b.com'),
+      _password = TextEditingController(text: '12345678');
 
- // final auth = FirebaseAuth.instance;
+  // final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      
-      debugShowCheckedModeBanner :false,
-      title: "appTitle",
-      home:Scaffold(  
-
-      appBar: AppBar(title: Text('Login'),),
-      body: Form(
-         key: key,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(  
-                  hintText: 'Email'
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "appTitle",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+          body: Form(
+            key: key,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    controller: _email,
+                  ),
                 ),
-                controller: _email,
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
+                    controller: _password,
+                  ),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          //  color: Theme.of(context).accentColor,
+                          child: Text('Signin'),
+                          onPressed: () {
+                            Auth.signIn(_email.text, _password.text);
+                          }),
+                    ])
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(hintText: 'Password'),
-                controller: _password,
-              ),
-              
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:[
-              ElevatedButton(
-              //  color: Theme.of(context).accentColor,
-                child: Text('Signin'),
-                onPressed: (){
-                    Auth.signIn(_email.text, _password.text);
-                    
-              }),
-             
-            ])
-          ],),
-      ),
-    ));
+          ),
+        ));
   }
 }
